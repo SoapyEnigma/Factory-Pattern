70542c7ec7c99371a5a5423b5d3ff8b755f032ee
SoapyEnigma <keithlekgolo@gmail.com>
2025-07-25 15:02:40 -0400

    Factory Exorcise

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Do you want a [2]motorcyle or a [4]car");
+            int input = 0;
+            while (!int.TryParse(Console.ReadLine(), out input) || input != 2 && input != 4)
+            {
+                Console.WriteLine("Please input 2 or 4");
+            }
+
+            IVehicle vehicle = VehicleFactory.GetVehicle(input);
+            vehicle.Drive();
         }
     }
 }
